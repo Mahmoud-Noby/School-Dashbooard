1e79c1c13e71e76529931fbccfad4a3d02a9a7c2
Mahmoud-Noby <mahmoudnoby146@outlook.com>
2019-09-15 17:15:48 +0200

    uploading

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').removeClass('.test');
+    });
+   
+});
